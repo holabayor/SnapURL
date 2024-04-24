8f4c86c754a6e25d339e1666b6b95ec0281fdea4
holabayor <liasu.olabayo@gmail.com>
2024-04-24 15:50:13 +0100

    updaing imports

--- client/src/components/CopyButton.jsx
+++ client/src/components/CopyButton.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Copy } from 'lucide-react';
-import Button from './ui/button';
+import Button from '@/components/ui/button';
 
 const CopyButton = ({ content, index }) => {
   const [copied, setCopied] = useState(false);
